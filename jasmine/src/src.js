7da7863a87500121978fdc6efa59c05f247be656
Robert Camp <robert.camp@outlook.com>
2013-10-18 17:12:25 -0600

    working on DOM manipulation

--- jasmine/src/src.js
+++ jasmine/src/src.js
@@ -1,35 +1,96 @@
 /* Takes an array and splits it into equal sized number of chunks*/
+
+//Original Chunk
 //@param arr     array
 //@param chunks  number
-var chunk = function chunk(arr, chunks){
-	var chunkedArrays = [];
-	var len = arr.length;
+var Chunk = (function(){
 
-	//gets the length each chunk needs to be by rounding down
-	var chunker = Math.floor(len/chunks);
+	var chunk = function chunk(arr, chunks){
+		var chunkedArrays = [];
+		var len = arr.length;
 
-	//the remainder tells how many chunks need an extra index
-	var extra = len%chunks;
+		//gets the length each chunk needs to be by rounding down
+		var chunker = Math.floor(len/chunks);
 
-	var begin=0
-	
-for(var i=0; i<chunks; i++){
-	
-	//while there needs to be extra indeces in chunks
-	if(extra > 0){
-		var end = (begin+chunker+1)
-		chunkedArrays.push(arr.slice(begin, end))
+		//the remainder tells how many chunks need an extra index
+		var extra = len%chunks;
+
+		var begin=0
+		
+		for(var i=0; i<chunks; i++){
+		
+		//while there needs to be extra indeces in chunks
+		if(extra > 0){
+			var end = (begin+chunker+1)
+			chunkedArrays.push(arr.slice(begin, end))
+				begin = end
+				extra--
+			}
+
+		//when the number of extra indexed chunks is through
+		else{
+			var end = (begin+chunker)
+			chunkedArrays.push(arr.slice(begin, end))
 			begin = end
-			extra--
 		}
 
-	//when the number of extra indexed chunks is through
-	else{
-		var end = (begin+chunker)
-		chunkedArrays.push(arr.slice(begin, end))
-		begin = end
+		}
+		return chunkedArrays
+	};
+
+	return {
+		chunk: chunk
 	}
 
+})();
+
+
+//DOM Chunk
+var DomChunk = (function(){
+
+	var domChunk = function chunk(people, groups){
+		var chunkedArrays = [];
+
+		var peopleArr = []
+		var peopleCounter = 0
+		for (var i=0; i<people; i++){
+			peopleArr.push(peopleCounter)
+			peopleCounter++
+		}
+
+		var len = peopleArr.length;
+
+		//gets the length each chunk needs to be by rounding down
+		var chunker = Math.floor(len/groups);
+
+		//the remainder tells how many chunks need an extra index
+		var extra = len%groups;
+
+		var begin=0
+		
+		for(var i=0; i<groups; i++){
+		
+		//while there needs to be extra indeces in chunks
+		if(extra > 0){
+			var end = (begin+chunker+1)
+			chunkedArrays.push(peopleArr.slice(begin, end))
+				begin = end
+				extra--
+			}
+
+		//when the number of extra indexed chunks is through
+		else{
+			var end = (begin+chunker)
+			chunkedArrays.push(peopleArr.slice(begin, end))
+			begin = end
+		}
+
+		}
+		return chunkedArrays
+	};
+
+	return {
+		domChunk: domChunk
 	}
-	return chunkedArrays
-}
+
+})();
---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(function(){
+
+$(document).on('submit', '.userInputForm', function(e) {
+	e.preventDefault();
+	$peopleCont = $('.peopleCont')
+	var numPeople = Number($('.peopleInput').val());
+	var numGroup = Number($('.groupInput').val());
+	
+	if(!numPeople || !numGroup){
+		$('.userInputCont').append('<p class="formReminder">Please fill out both forms!');
+		$('.formReminder').fadeOut('slow', function(){
+			$(this).remove();
+		})
+	}
+	else{
+		$('.peopleInput').val('');
+		$('.groupInput').val('');
+		$peopleCont.html('')
+		var peopleArray = []
+		var peopleCount = 0;
+		var groupChunk = Chunk.chunk(peopleArray, numGroup)
+		console.log('chunk', groupChunk) //working
+		var groupCount = 0
+
+		for(var i=0; i<numGroup; i++){
+			$peopleCont.append('<div class="group" data-group='+groupCount+'></div>')
+			groupCount ++
+			console.log('works')
+		}
+
+
+		for(var i=0, len=Math.floor(numPeople/numGroup); i<len; i++){
+			$('.group').attr('data-group', i).append('<div class="person" data-person='+peopleCount+'>Person!</div>')
+			peopleArray.push(peopleCount)
+			peopleCount ++
+		};
+
+	
+
+	}
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
